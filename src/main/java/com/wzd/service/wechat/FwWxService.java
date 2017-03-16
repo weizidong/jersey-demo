60b2d7eb9c847b12d2a84ab0d2c72830c2352f2b
weizidong <weizidong@ichuangye.cn>
2017-03-16 12:43:51 +0800

    ...

--- src/main/java/com/wzd/service/wechat/FwWxService.java
+++ src/main/java/com/wzd/service/wechat/FwWxService.java
@@ -6,11 +6,14 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 
+import com.wzd.service.wechat.base.MsgType;
+import com.wzd.service.wechat.event.Event;
+import com.wzd.service.wechat.msg.WxMsgReceiver;
 import com.wzd.service.wechat.utils.AesException;
 import com.wzd.service.wechat.utils.WXBizMsgCrypt;
+import com.wzd.service.wechat.utils.WeChatXmlUtil;
 import com.wzd.utils.Configs;
 import com.wzd.utils.HttpUtils;
-import com.wzd.utils.JaxbUtil;
 import com.wzd.web.dto.exception.WebException;
 import com.wzd.web.dto.response.ResponseCode;
 import com.wzd.web.param.wechat.WechatMsg;
@@ -48,17 +51,12 @@ public class FwWxService {
 	 * @param xml
 	 * @param request
 	 */
-	public void push(String msg_signature, String timestamp, String nonce, String sReqData,
-			HttpServletRequest request) {
+	public void push(String msg_signature, String timestamp, String nonce, String sReqData, HttpServletRequest request) {
 		log.debug("接收到服务号推送的消息。。。");
 		// URL解码
 		String sReqMsgSig = HttpUtils.ParseUrl(msg_signature);
 		String sReqTimeStamp = HttpUtils.ParseUrl(timestamp);
 		String sReqNonce = HttpUtils.ParseUrl(nonce);
-		log.debug("sReqMsgSig=" + sReqMsgSig);
-		log.debug("sReqTimeStamp=" + sReqTimeStamp);
-		log.debug("sReqNonce=" + sReqNonce);
-		log.debug("sReqData=" + sReqData);
 		// 对用户回复的消息解密
 		String sMsg;
 		try {
@@ -67,9 +65,34 @@ public class FwWxService {
 			throw new WebException(ResponseCode.不允许此方法, "解密回复信息密文失败！");
 		}
 		log.debug("解密结果：" + sMsg);
-		// 处理解密结果
-		WechatMsg msg = JaxbUtil.converyToJavaBean(sMsg, WechatMsg.class);
-		log.debug("解析结果：" + msg);
+		// 解析xml
+		WechatMsg msg = WeChatXmlUtil.xmlToBean(sMsg, WechatMsg.class);
+		switch (msg.getMsgType().toLowerCase()) {
+		case MsgType.TEXT: // 文本消息处理
+			WxMsgReceiver.text(msg);
+			break;
+		case MsgType.IMAGE: // 图片消息处理
+			// TODO 图片消息处理
+			break;
+		case MsgType.VOICE: // 语音消息处理
+			// TODO 语音消息处理
+			break;
+		case MsgType.VIDEO: // 视频消息处理
+			// TODO 视频消息处理
+			break;
+		case MsgType.SHORTVIDEO: // 小视频消息处理
+			// TODO 小视频消息处理
+			break;
+		case MsgType.LOCATION: // 地理位置消息处理
+			// TODO 地理位置消息处理
+			break;
+		case MsgType.LINK: // 链接消息处理
+			// TODO 链接消息处理
+			break;
+		case MsgType.EVENT: // 事件处理
+			Event.push(msg);
+			break;
+		}
 	}
 
 	/**
