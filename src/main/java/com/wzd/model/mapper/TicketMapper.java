f5a60d778a8c3ac307adec6972eb9ecff66c9262
weizidong <weizidong@ichuangye.cn>
2017-03-24 11:27:04 +0800

    加入webSocket

---
+++ src/main/java/com/wzd/model/mapper/TicketMapper.java
@@ -0,0 +1,7 @@
+package com.wzd.model.mapper;
+
+import com.wzd.model.entity.Ticket;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface TicketMapper extends Mapper<Ticket> {
+}
---
+++ src/main/java/com/wzd/model/mapper/WelfareMapper.java
@@ -0,0 +1,7 @@
+package com.wzd.model.mapper;
+
+import com.wzd.model.entity.Welfare;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface WelfareMapper extends Mapper<Welfare> {
+}
