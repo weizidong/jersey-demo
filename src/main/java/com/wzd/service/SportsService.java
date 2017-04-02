01ca81c64439b966944f692952a35e73a6ff3303
weizidong <weizidong@ichuangye.cn>
2017-04-02 17:15:14 +0800

    定义健身活动接口

--- src/main/java/com/wzd/service/SportsService.java
+++ src/main/java/com/wzd/service/SportsService.java
@@ -1,11 +1,17 @@
 package com.wzd.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.github.pagehelper.PageInfo;
 import com.wzd.model.dao.SportsDao;
 import com.wzd.model.entity.Admin;
 import com.wzd.model.entity.Sports;
+import com.wzd.model.entity.User;
+import com.wzd.model.enums.DeleteType;
+import com.wzd.web.param.PageParam;
 
 /**
  * 健身活动业务
@@ -27,25 +33,31 @@ public class SportsService {
 	}
 
 	/**
+	 * 修改健身活动
+	 */
+	public Sports update(Sports s) {
+		// TODO 修改健身活动
+		return null;
+	}
+
+	/**
 	 * 开启或关闭健身活动
 	 */
-	public Sports pause(Sports s, Admin admin) {
+	public void pause(String id) {
 		// TODO 开启或关闭健身活动
-		return null;
 	}
 
 	/**
 	 * 删除健身活动
 	 */
-	public Sports delete(Sports s, Admin admin) {
+	public void delete(String id, DeleteType del) {
 		// TODO 删除健身活动
-		return null;
 	}
 
 	/**
 	 * 获取健身活动列表
 	 */
-	public Sports list(Sports s, Admin admin) {
+	public PageInfo<Sports> list(PageParam param) {
 		// TODO 获取健身活动列表
 		return null;
 	}
@@ -53,9 +65,23 @@ public class SportsService {
 	/**
 	 * 获取健身活动报名列表
 	 */
-	public Sports entryList(Sports s, Admin admin) {
+	public List<Sports> entryList(PageParam param) {
 		// TODO 获取健身活动报名列表
 		return null;
+	}
+
+	/**
+	 * 报名健身活动
+	 */
+	public void entry(Sports s, User user) {
+		// TODO 报名健身活动
+	}
+
+	/**
+	 * 签到健身活动
+	 */
+	public void sign(User user) {
+		// TODO 签到健身活动
 	}
 
 }
--- src/main/java/com/wzd/web/rest/api/SportsApi.java
+++ src/main/java/com/wzd/web/rest/api/SportsApi.java
@@ -1,19 +1,27 @@
 package com.wzd.web.rest.api;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.github.pagehelper.PageInfo;
 import com.wzd.model.entity.Admin;
 import com.wzd.model.entity.Sports;
+import com.wzd.model.entity.User;
+import com.wzd.model.enums.DeleteType;
 import com.wzd.service.SportsService;
 import com.wzd.web.dto.session.SessionUtil;
+import com.wzd.web.param.PageParam;
 
 /**
  * 运动接口
@@ -35,5 +43,68 @@ public class SportsApi {
 	@Path("/create")
 	public Sports create(Sports s, @Context HttpServletRequest request) {
 		return service.create(s, (Admin) SessionUtil.getUser(request));
+	}
+
+	/**
+	 * 修改健身活动
+	 */
+	@POST
+	@Path("/update")
+	public Sports update(Sports s) {
+		return service.update(s);
+	}
+
+	/**
+	 * 删除健身活动
+	 */
+	@DELETE
+	@Path("/delete/{id}/{del}")
+	public void update(@PathParam("id") String id, @PathParam("del") Integer del) {
+		service.delete(id, DeleteType.parse(del));
+	}
+
+	/**
+	 * 获取健身活动列表
+	 */
+	@POST
+	@Path("/list")
+	public PageInfo<Sports> list(PageParam param) {
+		return service.list(param);
+	}
+
+	/**
+	 * 暂停或开启健身活动
+	 */
+	@POST
+	@Path("/pause/{id}")
+	public void pause(@PathParam("id") String id) {
+		service.pause(id);
+	}
+
+	/**
+	 * 报名
+	 */
+	@POST
+	@Path("/entry")
+	public void entry(Sports s, @Context HttpServletRequest request) {
+		service.entry(s, (User) SessionUtil.getUser(request));
+	}
+
+	/**
+	 * 签到
+	 */
+	@POST
+	@Path("/sign")
+	public void sign(@Context HttpServletRequest request) {
+		service.sign((User) SessionUtil.getUser(request));
+	}
+
+	/**
+	 * 获取健身活动报名列表
+	 */
+	@POST
+	@Path("/entryList")
+	public List<Sports> entryList(PageParam param) {
+		return service.entryList(param);
 	}
 }
